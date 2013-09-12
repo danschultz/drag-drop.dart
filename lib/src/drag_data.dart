2d000e7a6ebc581b50ef7c7674b203ed3d1edd5f
Dan Schultz <schultz.t.dan@gmail.com>
2013-09-11 17:00:43 -0700

    Fix breaking SDK change with extending HashMap

--- lib/src/drag_data.dart
+++ lib/src/drag_data.dart
@@ -2,21 +2,21 @@ part of drag_drop;
 
 typedef Object _Data();
 
-class DragData extends HashMap<String, _Data> {
-  
+class DragData {
+  Map<String, _Data> _data;
+
   DragData();
-  
+
   bool contains(String type) {
-    return containsKey(type);
+    return _data.containsKey(type);
   }
-  
+
   Object get(String type) {
-    var data = this[type];
+    var data = _data[type];
     return data != null ? data() : null;
   }
-  
+
   void set(String type, _Data data) {
-    this[type] = data;
+    _data[type] = data;
   }
-  
 }
