20467e3fd20a0fa4f5c791aad70184638df3f3d4
Anton Astashov <anton.astashov@gmail.com>
2015-03-27 14:33:42 -0700

    Revert making dragEvent public

--- lib/src/drag_source.dart
+++ lib/src/drag_source.dart
@@ -3,9 +3,7 @@ part of drag_drop;
 typedef DragImage DragImageFactory(Element element, Point pointer);
 
 DragImage _dragImage;
-
 DragEvent _dragEvent;
-DragEvent get dragEvent => _dragEvent;
 
 class DragSource {
   static const String IS_DRAGGING = "is-dragging";
